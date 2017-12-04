1abee1753a2f5dbc3498892e831ecb8f4f772245
BT_1N3_26 <BT_1N3_26@BT_1N3_26.bktech.local>
2017-12-04 12:32:24 -0500

    tryit

--- Try/src/challenges.java
+++ Try/src/challenges.java
@@ -3,11 +3,19 @@ public class challenges {
 
 	public static void main(String[] args)
 	{
-		
+		Random rnd = new Random();
+		int
 	}
 	
-	
+	//this function sorts the array and returns the median
 	public static int challengeOne(int[] arr)
+	{
+		
+		return findMedian(arr);
+	}
+	
+	//this function finds the median of the array
+	public static int findMedian(int[] arr)
 	{
 		
 	}
