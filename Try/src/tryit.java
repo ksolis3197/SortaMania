8933b2889e3040e70a8c5b6c493f512afc9e4ecd
BT_1N3_25 <BT_1N3_25@BT_1N3_25.bktech.local>
2017-12-04 13:09:01 -0500

    median testing space

--- Try/src/tryit.java
+++ Try/src/tryit.java
@@ -1,4 +1,17 @@
 
 public class tryit {
+	public static void main(String[]args)
+	{
+		median(6);
+	}
+	public static void median(int i)
+	{
+		int sum = 0;
+		for(int x = 0; x<=i; x++)
+		{
+			sum + x;
+			
+		}
+	}
 
 }
