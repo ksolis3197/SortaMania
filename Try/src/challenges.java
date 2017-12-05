ca5f201f46d0080286386e4834afd972608579de
BT_1N3_26 <BT_1N3_26@BT_1N3_26.bktech.local>
2017-12-05 12:04:34 -0500

    challenges

--- Try/src/challenges.java
+++ Try/src/challenges.java
@@ -1,22 +1,55 @@
+import java.util.Random;
 
 public class challenges {
 
 	public static void main(String[] args)
 	{
-		Random rnd = new Random();
-		int
+		int[] arr = createArray(10);
+		printArray(arr);
+		int median = challengeOne(arr);
+		printArray(arr);
+		System.out.println("median: " + median);
 	}
 	
 	//this function sorts the array and returns the median
 	public static int challengeOne(int[] arr)
 	{
-		
+		SortUtilities.quickSort(arr, 0, arr.length-1);
 		return findMedian(arr);
 	}
 	
 	//this function finds the median of the array
 	public static int findMedian(int[] arr)
 	{
-		
+		int middle = 0;
+		if (arr.length%2 == 0)
+		{
+			middle = arr[(arr.length/2 + (arr.length/2 - 1))/2];
+		}
+		else
+		{
+			middle = arr[arr.length/2-1];
+		}
+		return middle;
+	}
+	
+	public static int[] createArray(int n)
+	{
+		Random rnd = new Random();
+		int[] arr = new int[n];
+		for (int i = 0; i < n; i++)
+		{
+			arr[i] = rnd.nextInt(10);
+		}
+		return arr;
+	}
+	
+	public static void printArray(int[] arr)
+	{
+		for (int num : arr)
+		{
+			System.out.print(num + " ");
+		}
+		System.out.println();
 	}
 }
